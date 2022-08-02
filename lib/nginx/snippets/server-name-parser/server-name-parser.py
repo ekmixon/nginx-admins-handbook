8de8f91bf125fba2b50158f8d17b359f3f0fb887
Sourcery AI <>
2022-08-02 19:28:31 +0000

    'Refactored by Sourcery'

--- lib/nginx/snippets/server-name-parser/server-name-parser.py
+++ lib/nginx/snippets/server-name-parser/server-name-parser.py
@@ -16,19 +16,22 @@ def main(argv):
 
         data = myfile.read()
 
-        s_server = re.findall('([ \#]*server\s\{[.\s\na-zA-Z\/0-9\;\_\{\:\#\-]*server_name\s[.\s\na-zA-Z\/0-9\~\^\.\*\-*\;\_\{\:\#]*%s[\s\n;][\s\S]*?(?<=\n)[\#\s]})' % domain, data, re.MULTILINE | re.DOTALL)
+        if s_server := re.findall(
+            '([ \#]*server\s\{[.\s\na-zA-Z\/0-9\;\_\{\:\#\-]*server_name\s[.\s\na-zA-Z\/0-9\~\^\.\*\-*\;\_\{\:\#]*%s[\s\n;][\s\S]*?(?<=\n)[\#\s]})'
+            % domain,
+            data,
+            re.MULTILINE | re.DOTALL,
+        ):
+            dash_f = '>' * 10
+            dash_l = '<' * 10
 
-        dash_f = '>' * 10
-        dash_l = '<' * 10
-
-        if not s_server:
-            print('\x1b[0;33;40m' + 'WARNING:' + '\x1b[0m' + ' The following domain could not be found.')
-
-        else:
             for line in s_server:
                 print('\x1b[0;33;40m' + dash_f + '\x1b[0m' + '\x1b[1;36;40m' + ' BEG ' + '\x1b[0;33;40m' + dash_f + '\x1b[0m')
                 print(line)
                 print('\x1b[0;33;40m' + dash_l + '\x1b[0m' + '\x1b[1;36;40m' + ' END ' + '\x1b[0;33;40m' + dash_l + '\x1b[0m' + '\n')
+
+        else:
+            print('\x1b[0;33;40m' + 'WARNING:' + '\x1b[0m' + ' The following domain could not be found.')
 
 if __name__ == "__main__":
   main(sys.argv[1:])
